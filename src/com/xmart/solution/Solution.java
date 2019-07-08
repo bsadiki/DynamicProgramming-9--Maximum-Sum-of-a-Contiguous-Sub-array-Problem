b44d89d57956c77befb7bd05cf01c3542cc2db87
bsadiki <pedrosadiki@gmail.com>
2019-07-08 12:03:37 +0100

    Dynamic programing solution

--- src/com/xmart/solution/Solution.java
+++ src/com/xmart/solution/Solution.java
@@ -2,5 +2,18 @@ package com.xmart.solution;
 
 public class Solution {
     public static void main(String[] args) {
+        int [] array =  {-2, -5, 6, -2, -3, 1, 5, -6} ;
+        System.out.println(maximumSumOfContiguousSubArray(array));
+    }
+
+    private static int maximumSumOfContiguousSubArray(int[] array) {
+        int[] dynamicTab = new int[array.length];
+        dynamicTab[0] = Math.max(array[0],0);
+        int max = dynamicTab[0];
+        for (int i = 1 ; i < array.length; i++){
+            dynamicTab[i] = Math.max(dynamicTab[i-1] + array[i],array[i]);
+            max = Math.max(max, dynamicTab[i]);
+        }
+        return max;
     }
 }
